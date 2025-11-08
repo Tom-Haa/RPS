1aacfc4958799f244a44d3d6a8244c2006873f78
Tom <186923819+Tom-Haa@users.noreply.github.com>
2025-11-08 19:28:56 +0100

    working ComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Moin")
+function getComputerChoice() {
+    const randomNum = Math.random();
+    if (randomNum < 0.33) {
+      return "rock";
+    } else if (randomNum < 0.66) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  }
+  
+
+console.log(getComputerChoice())
