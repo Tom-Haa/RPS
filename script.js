a9dd51966f882254659a857670556f6651b1bfc3
Tom <186923819+Tom-Haa@users.noreply.github.com>
2025-11-08 20:10:32 +0100

    all 9 if.else cases covered.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNum = Math.random();
     if (randomNum < 0.33) {
@@ -8,6 +11,46 @@ function getComputerChoice() {
       return "scissors";
     }
   }
+
+  function getHumanChoice(){
+    let choice = prompt("choose Rock, Paper or Scissors!")
+
+    return choice
+  }
+  
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+  
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats Scissors");
+      humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats Rock");
+      computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats Rock");
+      humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beats Paper");
+      computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beats Paper");
+      humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats Scissors");
+      computerScore++;
+    } else {
+      console.log("Invalid choice!");
+    }
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
   
 
-console.log(getComputerChoice())
+  console.log(humanSelection);
+  console.log(computerSelection);
